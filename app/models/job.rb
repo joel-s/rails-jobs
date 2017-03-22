4e0e1fb3bb8b53e7d622bda372b9fe6cd88b1a1a
Joel Sullivan <joel@jsullivan.us>
2017-03-22 13:40:30 -0400

    New Job page looks like wireframe, but needs styling

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,22 @@
 class Job < ActiveRecord::Base
   enum category: [:no_category, :cleaning, :painting, :yardwork]
+  def self.category_list
+    list = Job.categories.map { |e| [e.first, e.first.humanize] }
+    list[0][1] = "Select"
+    return list
+  end
+
   enum location: [:no_location, :boston, :cambridge, :somerville]
-  enum status: [:created, :assigned, :in_progress, :completed]
+  def self.location_list
+    list = Job.locations.map { |e| [e.first, e.first.humanize] }
+    list[0][1] = "Select"
+    return list
+  end
+
+  enum status: [:no_status, :created, :assigned, :in_progress, :completed]
+  def self.status_list
+    list = Job.statuses.map { |e| [e.first, e.first.humanize] }
+    list[0][1] = "Select"
+    return list
+  end
 end
