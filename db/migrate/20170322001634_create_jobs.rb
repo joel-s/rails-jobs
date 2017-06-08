9e2b4fdba718fba4a1fe87db6d913512a1164027
Joel Sullivan <joel-s@users.noreply.github.com>
2017-06-08 15:56:14 -0400

    Change database to Postgres for compatibility with Heroku

--- db/migrate/20170322001634_create_jobs.rb
+++ db/migrate/20170322001634_create_jobs.rb
@@ -2,8 +2,8 @@ class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
       t.string :poster
-      t.integer :category null: false
-      t.integer :location null: false
+      t.integer :category
+      t.integer :location
       t.text :description
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170327181841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string   "poster"
     t.integer  "category"
